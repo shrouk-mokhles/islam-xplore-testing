383febf2c41de5ed627f47d0b8f6627e4d03fe32
shrouk-mokhles <shroukm146@gmail.com>
2025-12-07 11:43:34 +0200

    uncomment the signup test

--- tests/auth/sign-up/sign-up.spec.js
+++ tests/auth/sign-up/sign-up.spec.js
@@ -1,55 +1,55 @@
-// import { test,expect } from "@playwright/test";
-// import {
-//   openSignUp,
-//   fillSignupForm,
-//   clickSubmit,
-//   isVerifying,
-//   registrationFailed,
-//   emailInvalid,
-//   invalidPassword,
-//   invalidEmail,
-//   verifyUser,
-//   userData,
-// } from "./sign-up.js";
+import { test,expect } from "@playwright/test";
+import {
+  openSignUp,
+  fillSignupForm,
+  clickSubmit,
+  isVerifying,
+  registrationFailed,
+  emailInvalid,
+  invalidPassword,
+  invalidEmail,
+  verifyUser,
+  userData,
+} from "./sign-up.js";
 
 
 
-// test.describe("signUp", () => {
-//   test("signup Successflly", async ({ page }) => {
-//     await openSignUp(page);
-//     await fillSignupForm(
-//       page,
-//       userData.name,
-//       userData.email,
-//       userData.password,
-//       userData.password_confirmation
-//     );
-//     await clickSubmit(page);
-//     await isVerifying(page);
-//     await verifyUser(page, userData);
+test.describe("signUp", () => {
+  test("signup Successflly", async ({ page }) => {
+    await openSignUp(page);
+    await fillSignupForm(
+      page,
+      userData.name,
+      userData.email,
+      userData.password,
+      userData.password_confirmation
+    );
+    await clickSubmit(page);
+    await isVerifying(page);
+    await verifyUser(page, userData);
 
-//   });
+  });
 
-//   test("signup with registered email", async ({ page }) => {
-//     await openSignUp(page);
-//     await fillSignupForm(
-//       page,
-//       "shrouk",
-//       "user@test.com",
-//       "Welcome2creiden*",
-//       "Welcome2creiden*"
-//     );
-//     await clickSubmit(page);
-//     await registrationFailed(page);
-//   });
+  test("signup with registered email", async ({ page }) => {
+    await openSignUp(page);
+    await fillSignupForm(
+      page,
+      "shrouk",
+      "user@test.com",
+      "Welcome2creiden*",
+      "Welcome2creiden*"
+    );
+    await clickSubmit(page);
+    await registrationFailed(page);
+  });
 
-//   test("signup with weak password", async ({ page }) => {
-//     await openSignUp(page);
-//     await invalidPassword(page);
-//   });
+  test("signup with weak password", async ({ page }) => {
+    await openSignUp(page);
+    await invalidPassword(page);
+  });
 
-//   test("signup with invalid email", async ({ page }) => {
-//     await openSignUp(page);
-//     await invalidEmail(page);
-//   });
-// });
+  test("signup with invalid email", async ({ page }) => {
+    await openSignUp(page);
+    await invalidEmail(page);
+  });
+});
